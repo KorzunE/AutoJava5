dd205cfbd253380d110af792a57e01f77db42b52
Evgenii Korzun <kurosaki08@mail.ru>
2024-01-17 08:33:28 +0300

    change appveyor 8

--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -1,4 +1,5 @@
 package ru.netology;
+
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -22,11 +23,11 @@ public class AppCardDeliveryTest {
     public void shouldSuccessDelivery() {
         open("http://localhost:9999");
         $(By.cssSelector("[data-test-id='city'] input")).setValue("Курск");
-        $(By.cssSelector("[data-test-id='name'] input")).setValue("Корзун Евгений");
-        $(By.cssSelector("[data-test-id='phone'] input")).setValue("+79991234567");
         String planningDate = generateDate(4, "dd.MM.yyyy");
         $(By.cssSelector("[data-test-id='date'] input")).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), (Keys.DELETE));
         $(By.cssSelector("[data-test-id='date'] input")).setValue(planningDate);
+        $(By.cssSelector("[data-test-id='name'] input")).setValue("Корзун Евгений");
+        $(By.cssSelector("[data-test-id='phone'] input")).setValue("+79991234567");
         $(By.cssSelector("[data-test-id='agreement']")).click();
         $(By.cssSelector("button.button")).click();
         $(By.cssSelector(".notification__content"))
